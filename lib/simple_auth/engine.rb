63f8a86a86a40198f6158e1552167b8869d93826
brye <bryewalks@gmail.com>
2019-12-21 23:06:42 -0600

    fixed gem name in initializer

--- lib/simple_auth/engine.rb
+++ lib/simple_auth/engine.rb
@@ -2,7 +2,7 @@ module SimpleAuth
   class Engine < ::Rails::Engine
     isolate_namespace SimpleAuth
     initializer "actualize_auth.load_helpers" do |app|
-      ActionController::Base.send :include, ActualizeAuth::Engine.helpers
+      ActionController::Base.send :include, SimpleAuth::Engine.helpers
     end
   end
 end
