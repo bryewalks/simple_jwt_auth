58e95f4ec087ab4a0ae07ec91e9a1b33a77d1288
brye <bryewalks@gmail.com>
2019-12-21 22:43:51 -0600

    added controller actions

--- app/controllers/simple_auth/users_controller.rb
+++ app/controllers/simple_auth/users_controller.rb
@@ -2,5 +2,24 @@ require_dependency "simple_auth/application_controller"
 
 module SimpleAuth
   class UsersController < ApplicationController
+    wrap_parameters :user, include: [:name, :email, :password, :password_confirmation]
+  
+    def create
+      user = User.new(user_params)
+  
+      if user.save
+        render json: {message: "User created successfully"}, status: :created
+      else
+        render json: {errors: user.errors.full_messages}, status: :bad_request
+      end
+    end
+
+    private
+
+    def user_params
+      params
+        .require(:user)
+        .permit(:name, :email, :password, :password_confirmation)
+    end 
   end
 end
