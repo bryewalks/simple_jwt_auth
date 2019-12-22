3c9e4771582da41bc2d8f998a912925aedd13b17
brye <bryewalks@gmail.com>
2019-12-21 23:05:05 -0600

    added initializer to include engine helpers in application controller

--- lib/simple_auth/engine.rb
+++ lib/simple_auth/engine.rb
@@ -1,5 +1,8 @@
 module SimpleAuth
   class Engine < ::Rails::Engine
     isolate_namespace SimpleAuth
+    initializer "actualize_auth.load_helpers" do |app|
+      ActionController::Base.send :include, ActualizeAuth::Engine.helpers
+    end
   end
 end
